93f70d09f397edd7d4e07689a370af64acc01e61
Silvan Sholla <silvansholla@gmail.com>
2020-02-26 20:21:26 +0200

    add restrictions feature

--- app/src/main/java/com/uowm/ekasdym/UserActivity.java
+++ app/src/main/java/com/uowm/ekasdym/UserActivity.java
@@ -37,7 +37,7 @@ public class UserActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,R.id.weekly_matches,R.id.rank,R.id.rules,R.id.clarification)
+                R.id.add_restriction,R.id.weekly_matches,R.id.rank,R.id.rules,R.id.clarification,R.id.about_ekasdym,R.id.about_us)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
