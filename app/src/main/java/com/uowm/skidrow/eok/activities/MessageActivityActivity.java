452fdf0289fc6692d07aafb9366d67d26cd63b2c
Silvan Sholla <silvansholla@gmail.com>
2020-06-11 20:25:47 +0300

    beautify

--- app/src/main/java/com/uowm/skidrow/eok/activities/MessageActivityActivity.java
+++ app/src/main/java/com/uowm/skidrow/eok/activities/MessageActivityActivity.java
@@ -40,6 +40,7 @@ public class MessageActivityActivity extends AppCompatActivity {
     private int buddy;
     private static final String MESSAGE = "chat_message";
     private String url = "";
+    public String messageText = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,19 +85,17 @@ public class MessageActivityActivity extends AppCompatActivity {
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String messageText = messageET.getText().toString();
+                messageText = messageET.getText().toString();
                 if (TextUtils.isEmpty(messageText)) {
                     return;
                 }
+                new MessageSend().execute();
 
                 ChatMessage chatMessage = new ChatMessage();
                 chatMessage.setId(122);//dummy
                 chatMessage.setMessage(messageText);
                 chatMessage.setDate(DateFormat.getDateTimeInstance().format(new Date()));
                 chatMessage.setMe(false);
-
-                messageET.setText("");
-
                 displayMessage(chatMessage);
             }
         });
@@ -237,6 +236,78 @@ public class MessageActivityActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_0), Toast.LENGTH_LONG);
                 toast.show();
                 finishAffinity();
+            }
+        }
+    }
+
+    public class MessageSend extends AsyncTask < String, String, String > {
+        private ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MessageActivityActivity.this);
+            pDialog.setMessage(getString(R.string.waiting_screen));
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String...args) {
+
+            DatabaseHelper myDb = new DatabaseHelper(MessageActivityActivity.this);
+            Cursor res = myDb.getAllData();
+            String user_id = "";
+            String safe_key = "";
+
+            if (res.getCount() != 0) {
+                while (res.moveToNext()) {
+                    user_id = res.getString(3);
+                    safe_key = res.getString(5);
+                }
+            }
+            res.close();
+
+            url = getString(R.string.server) + "sentMessage.php?sender_id=" + user_id + "&receiver_id=" + buddy + "&text_message=" + messageText + "&safe_key=" + safe_key;
+            JSONParser jParser = new JSONParser();
+            String st = jParser.getJSONFromUrl(url);
+
+            return st;
+        }
+
+
+        @Override
+        protected void onPostExecute(String json) {
+            final Handler handler = new Handler();
+            handler.postDelayed(() -> pDialog.dismiss(), 500);
+            messageET.setText("");
+            int error_code = 0;
+            JSONObject jobj = null;
+
+            try {
+                jobj = new JSONObject(json);
+                JSONObject jobj4 = jobj.getJSONObject("ERROR");
+                error_code = jobj4.getInt("error_code");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (error_code == 200) {
+                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_200), Toast.LENGTH_LONG);
+                toast.show();
+            } else if (error_code == 403) {
+                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_403), Toast.LENGTH_LONG);
+                toast.show();
+                MessageActivityActivity.this.finishAffinity();
+            } else if (error_code == 201) {
+                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_201), Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_0), Toast.LENGTH_LONG);
+                toast.show();
+                MessageActivityActivity.this.finishAffinity();
             }
         }
     }
