246fd3128f2dfa51c3e71990c7c343ea7450bd7f
Silvan Sholla <silvansholla@gmail.com>
2020-06-11 19:53:48 +0300

    chat system added

--- app/src/main/java/com/uowm/skidrow/eok/activities/MessageActivityActivity.java
+++ app/src/main/java/com/uowm/skidrow/eok/activities/MessageActivityActivity.java
@@ -9,63 +9,122 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Html;
+import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.uowm.skidrow.eok.R;
+import com.uowm.skidrow.eok.adapters.MessageAdapter;
 import com.uowm.skidrow.eok.database.DatabaseHelper;
+import com.uowm.skidrow.eok.model.ChatMessage;
 import com.uowm.skidrow.eok.utilities.JSONParser;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MessageActivityActivity extends AppCompatActivity {
 
+    private EditText messageET;
+    private ListView messagesContainer;
+    private Button sendBtn;
+    private MessageAdapter adapter;
+    private ArrayList<ChatMessage> chatHistory = new ArrayList<ChatMessage>();
+    private int buddy;
+    private static final String MESSAGE = "chat_message";
     private String url = "";
-    private int id;
-    private String text_message;
-    EditText text;
-    Button bt;
-    DateFormat formatter;
-
-
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.message_edit);
-
-        formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        text = findViewById(R.id.password);
-        bt = findViewById(R.id.sent);
+        setContentView(R.layout.activity_chat_on_activity);
         Intent intent = getIntent();
-        id = intent.getIntExtra("id", 0);
+        buddy = intent.getIntExtra("id", 0);
         String name_surname = intent.getStringExtra("name_surname");
-
+        new JSONParse().execute();
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorPrimary)));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_left);
-        getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF'>"+getString(R.string.to)+" : " + name_surname + "</font>"));
-
-
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String temp = "" + text.getText();
-                text_message = temp.replaceAll(" ", "%20");
-                new JSONParse().execute();
-            }
-        });
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF' style=\"text-align:center;\">" + name_surname + "</font>"));
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initControls() {
+        messagesContainer = (ListView) findViewById(R.id.messagesContainer);
+        messageET = (EditText) findViewById(R.id.messageEdit);
+        sendBtn = (Button) findViewById(R.id.chatSendButton);
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
+        //companionLabel.setText("My Buddy");
+
+        loadDummyHistory();
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageET.getText().toString();
+                if (TextUtils.isEmpty(messageText)) {
+                    return;
+                }
+
+                ChatMessage chatMessage = new ChatMessage();
+                chatMessage.setId(122);//dummy
+                chatMessage.setMessage(messageText);
+                chatMessage.setDate(DateFormat.getDateTimeInstance().format(new Date()));
+                chatMessage.setMe(false);
+
+                messageET.setText("");
+
+                displayMessage(chatMessage);
+            }
+        });
+
+
+    }
+
+    public void displayMessage(ChatMessage message) {
+        adapter.add(message);
+        adapter.notifyDataSetChanged();
+        scroll();
+    }
+
+    private void scroll() {
+        messagesContainer.setSelection(messagesContainer.getCount() - 1);
+    }
+
+    private void loadDummyHistory(){
+
+        adapter = new MessageAdapter(MessageActivityActivity.this, new ArrayList<ChatMessage>());
+        messagesContainer.setAdapter(adapter);
+
+        for(int i=0; i<chatHistory.size(); i++) {
+            ChatMessage message = chatHistory.get(i);
+            displayMessage(message);
+        }
+
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -73,7 +132,7 @@ public class MessageActivityActivity extends AppCompatActivity {
         return true;
     }
 
-    public class JSONParse extends AsyncTask < String, String, String > {
+    public class JSONParse extends AsyncTask<String, String, String> {
         private ProgressDialog pDialog;
 
         @Override
@@ -82,13 +141,13 @@ public class MessageActivityActivity extends AppCompatActivity {
             pDialog = new ProgressDialog(MessageActivityActivity.this);
             pDialog.setMessage(getString(R.string.waiting_screen));
             pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
+            pDialog.setCancelable(false);
             pDialog.show();
 
         }
 
         @Override
-        protected String doInBackground(String...args) {
+        protected String doInBackground(String... args) {
 
             DatabaseHelper myDb = new DatabaseHelper(MessageActivityActivity.this);
             Cursor res = myDb.getAllData();
@@ -103,9 +162,9 @@ public class MessageActivityActivity extends AppCompatActivity {
             }
             res.close();
 
-            url = getString(R.string.server) + "sentMessage.php?sender_id=" + user_id + "&receiver_id=" + id + "&text_message=" + text_message + "&safe_key=" + safe_key;
+            url = getString(R.string.server)+"chatMessages.php?id="+user_id+"&safe_key="+safe_key+"&buddy="+buddy;
             JSONParser jParser = new JSONParser();
-            String st = jParser.getJSONFromUrl(url);
+            String st =jParser.getJSONFromUrl(url);
 
             return st;
         }
@@ -113,37 +172,71 @@ public class MessageActivityActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String json) {
-            text.setText("");
-            final Handler handler = new Handler();
-            handler.postDelayed(() -> pDialog.dismiss(), 500);
 
-            int error_code = 0;
+            chatHistory.clear();
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    pDialog.dismiss();
+                }
+            }, 500);
+
+            int error_code=0;
             JSONObject jobj = null;
 
 
             try {
                 jobj = new JSONObject(json);
                 JSONObject jobj4 = jobj.getJSONObject("ERROR");
-                error_code = jobj4.getInt("error_code");
+                error_code=jobj4.getInt("error_code");
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-            if (error_code == 200) {
-                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_200), Toast.LENGTH_LONG);
-                toast.show();
-            } else if (error_code == 403) {
+            if(error_code==200) {
+                try {
+
+                    JSONArray jsonArray = jobj.getJSONArray(MESSAGE);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        int id = obj.getInt("id");
+                        String set_me = obj.getString("set_me");
+                        String text_message = obj.getString("text_message");
+                        String date_time = obj.getString("date_time");
+
+                        ChatMessage msg = new ChatMessage();
+                        msg.setId(id);
+                        msg.setMe(Boolean.parseBoolean(set_me));
+                        msg.setMessage(text_message);
+                        msg.setDate(date_time);
+                        chatHistory.add(msg);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                initControls();
+
+            }
+            else if (error_code==403)
+            {
                 Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_403), Toast.LENGTH_LONG);
                 toast.show();
-                MessageActivityActivity.this.finishAffinity();
-            } else if (error_code == 201) {
-                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_201), Toast.LENGTH_LONG);
+                finishAffinity();
+            }
+            else if (error_code==204)
+            {
+                Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_204), Toast.LENGTH_LONG);
                 toast.show();
-            } else {
+                initControls();
+            }
+            else
+            {
                 Toast toast = Toast.makeText(MessageActivityActivity.this, getString(R.string.error_code_0), Toast.LENGTH_LONG);
                 toast.show();
-                MessageActivityActivity.this.finishAffinity();
+                finishAffinity();
             }
         }
     }
